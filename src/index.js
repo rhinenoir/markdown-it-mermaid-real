84dbc31443a3ca051523036a82467e5c08e0ef3a
GARENFEATHER <476217804@qq.com>
2018-03-04 00:36:38 -0800

    fuck

--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
-import mermaid from 'mermaid'
+import mermaid  from 'mermaid'
+import mermaidAPI  from 'mermaid/src/mermaidAPI'
+import crypto  from 'crypto'
 
-const mermaidChart = (code) => {
-  try {
-    mermaid.parse(code)
-    return `<div class="mermaid">${code}</div>`
-  } catch ({ str, hash }) {
-    return `<pre>${str}</pre>`
-  }
+const mermaidNode = {code:"", id:"", content:""}
+
+const config = {
+    logLevel: 5,
+    startOnLoad: false,
+    arrowMarkerAbsolute: false,
+    flowchart: {},
+    sequenceDiagram: {},
+    gantt: {
+        titleTopMargin: 25,
+        barHeight: 20,
+        barGap: 4,
+        topPadding: 50,
+        leftPadding: 75,
+        gridLineStartPadding: 35,
+        fontSize: 11,
+        fontFamily: '"Open-Sans", "sans-serif"',
+        numberSectionStyles: 3,
+        axisFormatter: [
+            // Within a day
+            ['%I:%M', d => d.getHours()],
+            // Monday a week
+            ['w. %U', d => d.getDay() === 1],
+            // Day within a week (not monday)
+            ['%a %d', d => d.getDay() && d.getDate() !== 1],
+            // within a month
+            ['%b %d', d => d.getDate() !== 1],
+            // Month
+            ['%m-%y', d => d.getMonth()],
+        ],
+    },
+    classDiagram: {},
+    gitGraph: {},
+    info: {}
+}
+
+function callback(svgGraph) {
+    console.log(svgGraph)
+    mermaidNode.content=svgGraph
+}
+const mermaidChart = () => {
+    try {
+        mermaid.parse(mermaidNode.code)
+        crypto.randomBytes(8,function(err, buffer) {
+            const svgId = buffer.toString('hex')
+        })
+        mermaidAPI.render(svgId, mermaidNode.code, callback)
+    } catch ({ str, hash }) {
+        mermaidNode.content = `<pre>${mermaidNode.code}</pre>`
+    }
 }
 
 const MermaidPlugin = (md) => {
-  md.mermaid = mermaid
-  mermaid.loadPreferences = (preferenceStore) => {
-    let mermaidTheme = preferenceStore.get('mermaid-theme')
-    if (mermaidTheme === undefined) {
-      mermaidTheme = 'default'
+    md.mermaid = mermaid
+    const temp = md.renderer.rules.fence.bind(md.renderer.rules)
+    md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+        const token = tokens[idx]
+        const code = token.content.trim()
+        if (token.info === 'mermaid') {
+            mermaidNode.code = code
+            mermaidChart()
+            return mermaidNode.content
+        }
+        return temp(tokens, idx, options, env, slf)
     }
-    let ganttAxisFormat = preferenceStore.get('gantt-axis-format')
-    if (ganttAxisFormat === undefined) {
-      ganttAxisFormat = '%Y-%m-%d'
-    }
-    mermaid.initialize({
-      theme: mermaidTheme,
-      gantt: { axisFormatter: [
-        [ganttAxisFormat, (d) => {
-          return d.getDay() === 1
-        }]
-      ]}
-    })
-    return {
-      'mermaid-theme': mermaidTheme,
-      'gantt-axis-format': ganttAxisFormat
-    }
-  }
-
-  const temp = md.renderer.rules.fence.bind(md.renderer.rules)
-  md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-    const token = tokens[idx]
-    const code = token.content.trim()
-    if (token.info === 'mermaid') {
-      return mermaidChart(code)
-    }
-    const firstLine = code.split(/\n/)[0].trim()
-    if (firstLine === 'gantt' || firstLine === 'sequenceDiagram' || firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) {
-      return mermaidChart(code)
-    }
-    return temp(tokens, idx, options, env, slf)
-  }
 }
 
-module.exports = MermaidPlugin
+export default MermaidPlugin
